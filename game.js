8dc75c910a6435fa64f8a47553bc0f544586c3d3
Olga Azhel <olga.azhel@gmail.com>
2023-02-06 22:36:37 -0500

    start to put all together

--- game.js
+++ game.js
@@ -1,31 +1,49 @@
 // Turn of each game will have different phases. Behavior of controllers will depend on phase of the turn.
 // Phases
-    // Function mandatoryJumps runs if false, function findActivePieces runs adds event listeners to active pieces, changes phase to choosePiece, sets clickCounter variable to 0
-    // choosePiece
-        // Player clicks on active piece, call availableMoves, the function should add event listeners to available empty cells, highlight cells for the move/jump, change phase to makeAmove/ makeAJump
-    // makeAmove
-        // player clicks on another active piece:
-            // remove empty cell highlight
-            // add highlight on another empty cell according to a new piece choice
-        // player clicks on highlighted empty cell
-            // change phase to moveComplete
-            // capture current piece cell id
-            // capture new cell id
-            // call function that will update board state - current value change to 0,
-            // new value change to turn or turn*2
-            // remove event listeners from all cells and pieces
-    // makeAjump
+// Function mandatoryJumps runs if false, function findActivePieces runs adds event listeners to active pieces, changes phase to choosePiece, sets clickCounter variable to 0
+// choosePiece
+// Player clicks on active piece, call availableMoves, the function should add event listeners to available empty cells, highlight cells for the move/jump, change phase to makeAmove/ makeAJump
+// makeAmove
+// player clicks on another active piece:
+// remove empty cell highlight
+// add highlight on another empty cell according to a new piece choice
+// player clicks on highlighted empty cell
+// change phase to moveComplete
+// capture current piece cell id
+// capture new cell id
+// call function that will update board state - current value change to 0,
+// new value change to turn or turn*2
+// remove event listeners from all cells and pieces
+// makeAjump
 //}
+// define([
+//     'require',
+//     'render'
+// ], function (require, render) {
+//     'use strict';
+//     let idBoard = render.boardOfIds
+//     console.log(idBoard)
+
+//     function handleMove(evt) {
+//         let checkBoard = boardOfIds(board)
+//         console.log("this is checkBoard",checkBoard)
+//         mandatoryJumps(checkBoard)
+//     }
+
+//     handleMove()
 
 
 
 
 
 
-define(function () {
 
-    return {
-        // add here what to export
+// })
 
-    }
-});
+// define(function () {
+
+//     return {
+//         // add here what to export
+//         handleMove
+//     }
+// });
--- move.js
+++ move.js
@@ -1,19 +1,214 @@
 define([
     'require',
     'render',
-    'active-pieces',
     'game'
 ], function (require, render) {
     'use strict';
+    // render function must use cellState object and board array to update board view according to current game state
 
+    const cellState = {
+        '1': { 'class': 'blue', 'html': "<span></span>", 'activePiece': 'blueA' },
+        '-1': { 'class': 'yellow', 'html': "<span></span>", 'activePiece': 'yellowA' },
+        '2': { 'class': 'blueKing', 'html': "<span></span>", 'activePiece': 'blueA' },
+        '-2': { 'class': 'yellowKing', 'html': "<span></span>", 'activePiece': 'yellowA' },
+        '0': { 'active': 'emptyCellA' },
+        's': { 'class': 'sleeping' }
+    }
+
+    let board = [
+        [0, 1, 0, 1, 0, 1, 0, 1],
+        [1, 0, 0, 0, 0, 0, 1, 0],
+        [0, 1, 0, 2, 0, 1, 0, 1],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, -1, 0, 0, 0, 0, 0, -1],
+        [-1, 0, -1, 0, -2, 0, -1, 0],
+        [0, -1, 0, 0, 0, -1, 0, -1],
+        [-1, 0, -1, 0, -1, 0, -1, 0]
+    ]
+    let turn = -1
 
     // need for guard option -  an array of cells divs only
     let cells = [...document.querySelectorAll('#board > div')]
     // This event listener should be set up on array of els returned from  findActivPieces()
-    document.getElementById('board').addEventListener('click', allowedMoves)
+    // document.getElementById('board').addEventListener('click', allowedMoves)
 
-    let highlightCells = []
-    console.log("CURRENTLY HIGHLIGHTED CELLS :")
+    //////////////// game state variables//////////////
+
+    let highlightCells = [] // cells that should be highlighted
+    let mustJumpPieces = [] // pieces that must jump
+    let currentMoveActivePieces = []
+    /////////////////// Functions ////////////////////
+
+    function render() {
+        renderPieces()
+
+        // function highlightPieces()
+    }
+
+    function renderPieces() {
+
+        // iterating through board array to acces value and update the view accordingly
+        board.map((rowArr, rowIdx) => {
+            let currentId
+            let r = `${rowIdx}`
+            rowArr.map((cell, colIdx) => {
+                if (cell === 0) return
+                let c = `${colIdx}`
+                currentId = 'r' + r + 'c' + c
+                // find element with current id in the HTML
+                let element = document.getElementById(currentId)
+                // check value of it in board array
+                let value = `${cell}`
+                // refer to cellState object to get class and inner HTML from it
+                let newHTML = cellState[value]["html"]
+                let newClass = cellState[value]["class"]
+                // adding piece element html 
+                element.innerHTML = newHTML
+                // applying class 
+                element.firstChild.classList.add(newClass)
+            })
+
+        })
+    }
+
+    render()
+
+
+    console.log(turn)
+    console.log(cellState[turn].activePiece)
+    function highlightPieces(mjArray) {
+        console.log("highlightPieces run...")
+        // mandatoryJumps must run before each move
+        // mandatoryJumps check all current turn pieces,  
+        // if mandatiryJumps returns array with length more than 0, this function should return the same array as result
+        if (mjArray) {
+            console.log("there is mandatory Jump:")
+            mjArray.forEach((id) => {
+                let span = document.getElementById(id)
+                console.log(span)
+                span.addEventListener('click', allowedMoves)
+            })
+            return pieceHighlighter(mjArray)
+        }
+        // Function will look for pieces that player can choose from to make a new move or to continue jump 
+        let activePieces = []
+        // To be active in this turn piece must:
+        // have value of turn or turn * 2
+        // if value of cell = turn
+
+        board.forEach((rowArr, rowIdx) => {
+            let currentId
+            let r = `${rowIdx}`
+            rowArr.map((cell, colIdx) => {
+                if (cell === 0) return
+                if (cell === turn) {
+                    let addId = cellState[turn].activePieces
+                    //have at least one free spot diagonally forward
+                    // this free spot must have coordinates within the board
+                    let c = `${colIdx}`
+                    currentId = 'r' + r + 'c' + c
+                    let checkRow = rowIdx + turn  //3 + -1 --> 2
+                    // creating an array of two possible for move cells to check 
+                    let checkCellsVals = [board[checkRow][colIdx + 1], board[checkRow][colIdx - 1]]
+                    // saving these cells' ids
+                    let checkCellsIds = [`r${checkRow}c${colIdx + 1}`, `r${checkRow}c${colIdx - 1}`]
+                    // filtering the array to find a cell that has a value 0. If any true,we can mark this cell as active
+                    checkCellsVals.map((maybeEmptyCell, idx) => {
+                        if (maybeEmptyCell === 0) {
+                            activePieces.push(currentId)
+                            let highlightEl = document.getElementById(currentId).childNodes[0]
+
+                            highlightEl.setAttribute('id', addId)
+                            console.log(highlightEl)
+                        }
+                    })
+                } else if (cell === turn * 2) {
+                    let addId = cellState[turn].active
+                    let c = `${colIdx}`
+                    currentId = 'r' + r + 'c' + c
+                    console.log("checking this for availabale moves:", currentId)
+                    let checkRows = [[rowIdx + turn], [rowIdx - turn]]
+                    // creating an array of two possible for move cells to check 
+                    let checkCellsVals = []
+                    // saving these cells' ids
+                    let checkCellsIds = []
+                    checkRows.map((el) => {
+                        checkCellsVals.push(board[el][colIdx + 1])
+                        checkCellsIds.push(`r${el}c${colIdx + 1}`)
+                        checkCellsVals.push(board[el][colIdx - 1])
+                        checkCellsIds.push(`r${el}c${colIdx - 1}`)
+                    })
+
+                    // filtering the array to find a cell that has a value 0 and pushing into available moves
+                    checkCellsVals.map((cell, idx) => {
+                        if (cell === 0) {
+                            activePieces.push(currentId)
+                        }
+                    })
+
+                }
+            })
+        })
+        pieceHighlighter(activePieces)
+        // adding event listeners to activePieces
+        activePieces.forEach((id) => {
+            let span = document.getElementById(id)
+            console.log(span)
+            span.addEventListener('click', allowedMoves)
+        })
+        currentMoveActivePieces = activePieces
+        return activePieces
+
+    }
+
+    function pieceHighlighter(arr) {
+        arr.forEach((elId) => {
+            let addId = cellState[turn].activePiece
+            let highlightEl = document.getElementById(elId).childNodes[0]
+            highlightEl.setAttribute('id', addId)
+        })
+    }
+ 
+
+    
+    //main logic control of the game
+    function handleMove(evt) {
+        // looking for mandatory moves on the board
+        let checkBoard = boardToIDarray(board)
+        mandatoryJumps(checkBoard)
+
+        // if mustJumpPieces.length > 0 - highlight those cells
+        if (mustJumpPieces.length > 0) {
+            highlightPieces(mustJumpPieces)
+        } else {
+            highlightPieces() //add event listeners to active pieces
+        }  
+    // Player clicks on a piece calling event allowedMoves event listener 
+ 
+    }
+
+    function processMove() {
+        console.log("PROCESSING A NEW MOVE")
+    }
+
+
+    function boardToIDarray(array) {
+        let idArray = []
+        array.forEach((row, i) => {
+            
+            row.forEach((cell, idx) => {
+                let newId = `r${i}c${idx}`
+                idArray.push(newId)
+            })
+            
+        })
+        // console.log("This is ID board", idBoard)
+        return idArray
+
+    }
+    handleMove()
+ 
+
 
 
     function allowedMoves(evt) {
@@ -86,15 +281,29 @@ define([
             // updating highlighted cells array. Will need to remove highlight once another event occurs 
             highlightCells = availableMoves
             emptyCellHighlighter(availableMoves)
+            availableMoves.forEach((id) => {
+                let span = document.getElementById(id)
+                console.log(span)
+                span.addEventListener('click', processMove)
+            })
+
             return availableMoves
         } else {
             console.log("YOU MUST JUMP:", mJumps)
             highlightCells = mJumps
             emptyCellHighlighter(mJumps)
+            availableMoves.forEach((id) => {
+                let span = document.getElementById(id)
+                console.log(span)
+                span.addEventListener('click', processMove)
+            })
             return mJumps
         }
 
+
     }
+
+
 
     function emptyCellHighlighter(array) {
         array.forEach((elId) => {
@@ -122,7 +331,7 @@ define([
 
         // function have to check if any mandatory jumps must be made // accepting array of pieces to check. 
         // different behavior for regular and kings pieces
-
+        let haveToJumpPieces = []
         let mandatoryJumpsArr = []
         for (let i = 0; i < array.length; i++) {
             let curRowIdx = parseInt(array[i][1])
@@ -153,6 +362,7 @@ define([
                 checkCellsIfEmpty.forEach((el) => {
                     if (checkOppCells[el] = turn * -1 && emptyCheck[el] === 0 && emptyCheck[el] !== undefined) {
                         mandatoryJumpsArr.push(emptyCheckIds[el])
+                        mustJumpPieces.push(array[i])
                     }
                 })
                 // if King
@@ -195,13 +405,15 @@ define([
                     // if cell at current index in array next cells belongs to opponent and same index in next +1 array is empty - this is a mandatory jump
                     if (checkOppCells[el] = turn * -1 && emptyCheck[el] === 0 && emptyCheck[el] !== undefined) {
                         mandatoryJumpsArr.push(emptyCheckIds[el])
+                        mustJumpPieces.push(array[i])
 
                     }
                 })
             }
 
         }
-        console.log("mandatory jumps return", mandatoryJumpsArr)
+        
+        console.log("mandatory jumps return", mandatoryJumpsArr, mustJumpPieces)
         // function must return an array of indexes requiring a jump
         return mandatoryJumpsArr
 
@@ -217,6 +429,8 @@ define(function () {
         // add here what to export
         board,
         cells,
+        mandatoryJumps,
+        allowedMoves
 
 
     }
