1857b7f81418f5cb69d07cbf0c65bf0b247443d2
Olga Azhel <olga.azhel@gmail.com>
2023-02-09 23:34:18 -0500

    css styling add

--- move.js
+++ move.js
@@ -53,10 +53,30 @@ define([
     ]
 
     /////////////////// Functions ////////////////////
+    // function generateBoard() {
+    //     let newId = ''
+    //     let sectionBoard = document.getElementById('board')
+    //     board.forEach((row, rowIdx) => {
+    //         row.forEach((cell, cellIdx) => {
+    //             let newDiv = document.createElement('div')
+    //             newId = `r${rowIdx}c${cellIdx}`
+    //             console.log(newDiv)
+    //             newDiv.setAttribute('id', newId)
+    //             sectionBoard.appendChild(newDiv)
+    //             if (rowIdx % 2 === 0 && cellIdx % 2 === 1) {
+    //                 newDiv.style.backgroundColor = "rgb(122, 137, 181)"
+    //             } else if (rowIdx % 2 === 1 && cellIdx % 2 === 0) {
+    //                 newDiv.style.backgroundColor = "rgb(122, 137, 181)"
+    //             }
+    //         })
+    //     })
+    // }
+
 
     init()
     //start of the game
     function init() {
+        // generateBoard()
         if (moveCounter === 40) {
             checkTie()
         }
